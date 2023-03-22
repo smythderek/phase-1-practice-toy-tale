dda42de8a19edc1914bd9a838d279ad9fed6ace0
Derek Smyth <smyth.derek@gmail.com>
2023-03-22 09:31:09 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
@@ -13,3 +12,88 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", getAllToys());
+
+function getAllToys() {
+  fetch("http://localhost:3000/toys")
+  .then(res => res.json())
+  .then(toyDataAll => toyDataAll.forEach(toy => renderToy(toy)))
+};
+
+function renderToy(toy) {
+   console.log(toy);
+
+   const toyCard = document.createElement('div');
+   toyCard.className = 'card';
+   document.getElementById('toy-collection').appendChild(toyCard);
+   
+   const toyName = document.createElement('h2');
+   toyName.innerText = toy.name;
+   toyCard.appendChild(toyName);
+
+   const toyImage = document.createElement('img');
+   toyImage.src = toy.image;
+   toyImage.className = 'toy-avatar';
+   toyCard.appendChild(toyImage);
+   
+   const toyLikes = document.createElement('p');
+   let numLikes = toy.likes;
+   toyLikes.className = 'like_button';
+   toyLikes.innerText = `${numLikes} likes`
+   toyCard.appendChild(toyLikes);
+
+   const likeButton = document.createElement('btn');
+   likeButton.className = 'button';
+   likeButton.id = toy.id;
+   likeButton.innerText = "Like";
+   toyCard.appendChild(likeButton);
+
+   likeButton.addEventListener('click', function() {
+    numLikes++;
+    if (numLikes === 1) {
+      toyLikes.innerText = `${numLikes} like`
+    }
+    else toyLikes.innerText = `${numLikes} likes`
+    
+    toy.likes = numLikes;
+
+    updateLikeCount(toy);
+   })
+}
+
+function updateLikeCount(obj) {  
+  fetch(`http://localhost:3000/toys/${obj.id}`, { 
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(obj)
+  })
+  .then(res => res.json())
+  .then(toy => console.log(toy ))
+
+}
+
+document.addEventListener("DOMContentLoaded", toySubmitted()) 
+
+function toySubmitted(e) {
+  const form = document.getElementById("toy-form");
+  form.addEventListener('submit', function(e) { 
+    e.preventDefault();
+    let toyObj = {
+      name:e.target.name.value,
+      image:e.target.image.value,
+      likes:0
+    }
+    fetch("http://localhost:3000/toys", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(toyObj)
+    })
+    .then(res => res.json())
+    .then(json => renderToy(json))
+  })
+}
